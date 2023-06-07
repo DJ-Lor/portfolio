0215f9a218aa86184a2e3b74beaa7d45ab2747ca
Loreli <dejesus.loreli@gmail.com>
2023-06-07 22:54:46 +1000

    Renamed home file to Main.jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Main";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import "./index.css";
